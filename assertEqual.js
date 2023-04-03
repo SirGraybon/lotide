fb06e362c29942cdf98ef4261c70446a270c4e31
SirGraybon <graydonritchie@gmail.com>
2023-04-03 16:06:29 -0400

    added head.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //Function
 let assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertation passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertation passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertation failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertation failed: ${actual} !== ${expected}`});
   }
 };
 
