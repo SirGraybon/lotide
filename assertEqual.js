f5e3fc3bb18c51978e3f8505e9f218e587888851
SirGraybon <graydonritchie@gmail.com>
2023-04-03 16:08:27 -0400

    fiex es errors

--- assertEqual.js
+++ assertEqual.js
@@ -3,11 +3,11 @@ let assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertation passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertation failed: ${actual} !== ${expected}`});
+    console.log(`🛑🛑🛑 Assertation failed: ${actual} !== ${expected}`);
   }
 };
 
 // Test code
 assertEqual("Lighthouse Labs", "bootcamp");
 assertEqual(1,1);
-assertEqual("Good boy","good boy")
+assertEqual("Good boy","good boy");
