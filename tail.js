00a7f818b18be903ef3c3c65e2bc2589f9ee8bf3
SirGraybon <graydonritchie@gmail.com>
2023-04-03 16:32:51 -0400

    added tail function

---
+++ tail.js
@@ -0,0 +1,20 @@
+//Function
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertation failed: ${actual} !== ${expected}`);
+  }
+};
+
+let tail = function(input) {
+  let output = [];
+  for (let i = 1; i < input.length; i ++) {
+    output.push(input[i]);
+  }
+  return output.join("");
+};
+
+console.log(tail("hello"));
+assertEqual(tail("Hello"), "ello")
+assertEqual(tail("Hello").length, 4)
