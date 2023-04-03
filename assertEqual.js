c3a730d745f2e4afd913c1481e0615ec686b9abd
SirGraybon <graydonritchie@gmail.com>
2023-04-03 15:23:43 -0400

    added assertEquals

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+//Function
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertation passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertation failed: " + actual + " !== " + expected);
+  }
+};
+
+// Test code
+assertEqual("Lighthouse Labs", "bootcamp");
+assertEqual(1,1);
+assertEqual("Good boy","good boy")
