8ae23392fc1853c4ac3009626ed9224ecc222bd9
SirGraybon <graydonritchie@gmail.com>
2023-04-04 17:16:39 -0400

    function created and tested

---
+++ eqArray.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+
+//Functions
+// assesses the expected outcome of a function to the actual outcome
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertation failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqArrays = function(first, second) {
+  let a = first.join("");
+  let b = second.join("");
+  let out = (a === b);
+  //return out;
+  console.log(out);
+
+};
+
+//console.log(eqArrays(45,));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
