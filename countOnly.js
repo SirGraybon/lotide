07194478b7ffa9723d01954c330b692b83a7ebbe
SirGraybon <graydonritchie@gmail.com>
2023-04-05 19:05:46 -0400

    implementation of countOnly

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+//Assertation Function
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertation failed: ${actual} !== ${expected}`);
+  }
+};
+
+//countOnly
+let countOnly = function(allItems, itemsToCount) {
+  let attendance = {};
+  for (let item in itemsToCount) {
+    let count = 0;
+    //let count = [];
+    if (itemsToCount[item] === true) {
+      for (let i = 0; i < allItems.length; i ++) {
+        if (item === allItems[i]) {
+          count ++;
+          //count.push(item);
+          attendance[item] = count;
+        }
+      }
+    }
+  }
+  return attendance;
+};
+
+
+//test cases
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+// console.log(result1)
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
