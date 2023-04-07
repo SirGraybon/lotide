42abd2ab34f0db6d276cba8bea858d0026e8a77a
SirGraybon <graydonritchie@gmail.com>
2023-04-06 20:21:07 -0400

    logic for findKey.js implemented

---
+++ findKey.js
@@ -0,0 +1,34 @@
+//Assertation Function
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertation failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+let findKey = function(object, callBack) {
+  for (let keys in object) {
+    // console.log(object[keys])
+    // console.log(keys.stars)
+    // console.log(callBack(keys))
+
+    if (callBack(object[keys])) {
+      //console.log(object[keys]);
+      console.log(keys);
+      return keys;
+    }
+  }
+}
+
+
+//test cases
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
